111c543decc6f6350c0d4db6ebf2da50ba5fea73
ACCDOM01\M95806 <lsrr@tdcnet.dk>
2022-10-28 09:34:53 +0200

    rettet tekstbox tekst

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Hejsa");
+            MessageBox.Show("Hejsa 777777777");
         }
     }
 }
